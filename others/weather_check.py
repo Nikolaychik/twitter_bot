2bf66690a94a70e3a048b3a37a0af8c7bf8890a7
grishadz <griha96u@gmail.com>
2016-10-26 20:48:59 +0300

    add historyval

--- others/weather_check.py
+++ others/weather_check.py
@@ -7,3 +7,11 @@ def sinoptik_temperature_current_kiev():
     parsed_html_document = BeautifulSoup(markup=html)
     current_temp = parsed_html_document.find('p', class_='today-temp').text
     return current_temp
+
+def sinoptik_temperature_current_kiev_historyval():
+    html = requests.get('https://ua.sinoptik.ua/погода-київ').text
+    parsed_html_document = BeautifulSoup(markup = html)
+    current_temp = parsed_html_document.find('p', class_='infoHistoryval').text.split(':')[2][:5]
+    return  current_temp
+
+sinoptik_temperature_current_kiev_historyval()
