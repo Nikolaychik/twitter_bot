7f116e3efc1cbdcf7e096e95bada5aafadb0e92d
drvadus <dr.vadus@gmail.com>
2016-10-26 20:32:52 +0300

    min temp function added

--- others/weather_check.py
+++ others/weather_check.py
@@ -2,8 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def sinoptik_temperature_current_kiev():
+def sinoptik_temperature_current_kiev() -> object:
     html = requests.get('https://ua.sinoptik.ua/погода-київ').text
     parsed_html_document = BeautifulSoup(markup=html)
     current_temp = parsed_html_document.find('p', class_='today-temp').text
     return current_temp
+
+def sinoptik_today_min():
+    html = requests.get('https://ua.sinoptik.ua/погода-київ').text
+    parsed_html_document = BeautifulSoup(markup=html)
+    today_min = parsed_html_document.find('div', class_='min').text
+    return today_min
+
+sinoptik_today_min()
