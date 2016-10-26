937731b4080144d246a932a2af1dbcc66c45de39
Makedonsky <mashianov@gmail.com>
2016-10-26 18:55:30 +0300

    change example function

--- others/weather_check.py
+++ others/weather_check.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def sinoptik_weather_check():
+def sinoptik_temperature_current_check():
     html = requests.get('https://ua.sinoptik.ua/погода-київ').text
     parsed_html_document = BeautifulSoup(markup=html)
     current_temp = parsed_html_document.find('p', class_='today-temp').text
