e412d1b44e86f1ec1d78ae596cb99271a58b8fef
Sasha-P <sasha.pazyuk@gmail.com>
2016-10-26 20:56:41 +0300

    add get_info_day_light_down_time

--- others/weather_check.py
+++ others/weather_check.py
@@ -4,6 +4,18 @@ from bs4 import BeautifulSoup
 
 def sinoptik_temperature_current_kiev():
     html = requests.get('https://ua.sinoptik.ua/погода-київ').text
-    parsed_html_document = BeautifulSoup(markup=html)
+    parsed_html_document = BeautifulSoup(html, "lxml")
     current_temp = parsed_html_document.find('p', class_='today-temp').text
     return current_temp
+
+
+def get_info_day_light_down_time():
+    html = requests.get('https://ua.sinoptik.ua/погода-київ').text
+    parsed_html_document = BeautifulSoup(html, "lxml")
+    day_light_down_time = str(parsed_html_document.find('div', class_='infoDaylight').text).strip().split('Захід ')[1]
+    return day_light_down_time
+
+
+if __name__ == '__main__':
+    print(sinoptik_temperature_current_kiev())
+    print(get_info_day_light_down_time())
