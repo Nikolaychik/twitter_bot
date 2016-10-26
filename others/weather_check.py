d19fc28f8c2da345512f415fa2b9679c8d804ef6
lion <lion@MacBook-Pro-lion.local>
2016-10-26 20:46:17 +0300

    vadim: max temp

--- others/weather_check.py
+++ others/weather_check.py
@@ -7,3 +7,11 @@ def sinoptik_temperature_current_kiev():
     parsed_html_document = BeautifulSoup(markup=html)
     current_temp = parsed_html_document.find('p', class_='today-temp').text
     return current_temp
+
+def sinoptik_temperature_max_kiev():
+    html = requests.get('https://ua.sinoptik.ua/погода-київ').text
+    parsed_html_document = BeautifulSoup(markup=html)
+    current_temp = parsed_html_document.find('p', class_='infoHistoryval').text
+    return current_temp.split(" ")[2]
+
+sinoptik_temperature_max_kiev()
