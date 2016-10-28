b134572fd68d43fa34114f4dc2df405b23337d3c
drvadus <dr.vadus@gmail.com>
2016-10-28 13:45:05 +0300

    homework

--- twitter/api.py
+++ twitter/api.py
@@ -12,32 +12,7 @@ class TwitterBot:
         self.api = tweepy.API(auth)
         
     def update_status(self, status):
-        pass # api.update_status(status)
+        self.api.update_status(status)
 
-
-
-
-# configs = {
-#     'API KEY': 'FcuBGT2E2vSusk6rNlSfSUQCc',
-#     'API Secret': 'Lp96ATqrEP4rzoUCiZpBw5b7dXFbJoMCONDFwwuDADjL5S43C2',
-#     'Access token': '791307771924938752-K1ovtmZGXxasV25GAVsScGu5Mhoc5zF',
-#     'Access token secret': 'yY2GcZxokNenu88kCQ7F6vVXwhgHipElsXEomQi0TWGwX'
-# }
-#
-# auth = tweepy.OAuthHandler(consumer_key=configs['API KEY'],
-#                            consumer_secret=configs['API Secret'])
-# auth.set_access_token(configs['Access token'],
-#                       configs['Access token secret'])
-#
-# api = tweepy.API(auth)
-#
-# public_tweets = api.home_timeline()
-# for tweet in public_tweets:
-#     print(tweet.text)
-#
-# status = "Hello, World!"
-#
-# for x in range(10):
-#   status += random.choice(ascii.letters)
-#
-# api.update_status(status)
+    def home_timeline(self,):
+        self.api.home_timeline()
