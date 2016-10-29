6bbb745ea3af4450408287181648ddfb880159d1
drvadus <dr.vadus@gmail.com>
2016-10-29 16:24:28 +0300

    my configs added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from twitter import api
+import configs
 
-a = api.TwitterBot(api.configs)
+a = api.TwitterBot(configs.config_my)
 
 b = a.tweets_timeline()
 c = a.api.followers_ids()
--- twitter/api.py
+++ twitter/api.py
@@ -1,12 +1,5 @@
 import tweepy
 
-configs = {
-    'API KEY': 'FcuBGT2E2vSusk6rNlSfSUQCc',
-    'API Secret': 'Lp96ATqrEP4rzoUCiZpBw5b7dXFbJoMCONDFwwuDADjL5S43C2',
-    'Access token': '791307771924938752-K1ovtmZGXxasV25GAVsScGu5Mhoc5zF',
-    'Access token secret': 'yY2GcZxokNenu88kCQ7F6vVXwhgHipElsXEomQi0TWGwX'
-}
-
 
 class TwitterBot:
     def __init__(self, credentials):
